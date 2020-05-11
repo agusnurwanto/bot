9e1693393577c3cceb9e04edf26dba3ea135ba9d
agusnurwanto <agusnurwantomuslim@gmail.com>
2020-05-11 12:16:30 +0700

    tambah quran.com

---
+++ qurandotcom.js
@@ -0,0 +1,7 @@
+// add jquery
+
+jQuery('._32NvhYFTY8ARGfNWQ_-6hv').hide();
+jQuery('iframe').hide();
+jQuery('.indonesian').css('color', '#fff');
+jQuery('.pager').hide();
+jQuery('footer').hide();
