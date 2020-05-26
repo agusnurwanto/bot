a5d03b0141069fe1135026cff1997ba10d055c05
agusnurwanto <agusnurwantomuslim@gmail.com>
2020-05-27 05:41:18 +0700

    perbaikan ratio layar

--- qurandotcom.js
+++ qurandotcom.js
@@ -7,3 +7,5 @@ jQuery('.pager').hide();
 jQuery('footer').hide();
 jQuery('.montserrat').remove(); // hapus translate bahasa
 jQuery('._3Wvc2PyRmdtcwk1ww7wZ_A').closest('div').remove(); // hapus referensi youtube
+jQuery('.col-md-10.col-md-offset-1').attr('class', 'col-md-12'); // hapus margin
+jQuery('.col-md-11.col-sm-11').attr('class', 'col-md-10'); // perbesar layar agar ratio layar youtube bisa full 100%
